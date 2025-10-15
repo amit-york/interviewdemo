e7d164ae5a2e88276f00f5d865bfadd2ca453d44
amit-york <amitm@york.ie>
2025-10-16 01:06:06 +0530

    initial commit

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# name="World"
+# echo "Hello, $name!"
+num=10
+
+if [ $num -gt 5 ]; then
+echo "$num is greater than 5"
+else
+echo "$num is not greater than 5"
+fi
+
+
+
+# if [ $num -gt 5 ]; then
+
+# else
+
+# fi
